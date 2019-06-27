a6fe8de92a045c4651668390b4e33dec23d574a5
Himanshi <Himanshichamoli95>
2019-06-27 10:31:30 +0530

    commit

---
+++ events.tac.local/Controllers/EventsController.cs
@@ -0,0 +1,56 @@
+﻿using events.tac.local.Areas.Importer.Models;
+using Newtonsoft.Json;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace events.tac.local.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, string parentPath)
+        {
+            IEnumerable<Event> events = null;
+            var database = Sitecore.Configuration.Factory.GetDatabase("master");
+            var parentItem = database.GetItem(parentPath);
+            var templateID = new TemplateID(new ID("{3B9C5F33-3A68-4E8F-B635-FE8FFE981461}"));
+
+            using (new SecurityDisabler())
+            {
+                foreach(var ev in events)
+                {
+                    var name = ItemUtil.ProposeValidItemName(ev.ContentHeading);
+                    Item item = parentItem.Add(name, templateID);
+                    item.Editing.BeginEdit();
+                    item["ContentHeading"] = ev.ContentHeading;
+                    item.Editing.EndEdit();
+                }
+            }
+                string message = null;
+            using (var reader = new System.IO.StreamReader(file.InputStream))
+            {
+                var contents = reader.ReadToEnd();
+                try
+                {
+                    events = JsonConvert.DeserializeObject<IEnumerable<Event>>(contents);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ events.tac.local/Scripts/commentsForm.js
@@ -0,0 +1,26 @@
+﻿function createCommentItem(form,path)
+{
+    var service = new Itemservice({ url: '/sitecore/api/ssc/item' });
+    window.alert('thanks.Your message will show on the site shortly1');
+    var obj = {
+        ItemName: 'comment -' + form.name.value,
+        TemplateID: '{E8CCBF92-95D5-4855-90E4-9CD31EDDA475}',
+        Name: form.name.value,
+        Comment: form.comment.value
+
+    };
+    window.alert('thanks.Your message will show on the site shortly2');
+    service.create(obj)
+        .path(path)
+        .execute()
+        .then(function (item) {
+            form.name.value = form.comment.value = '';
+            window.alert('thanks.Your message will show on the site shortly');
+        })
+        .fail(function (err) {
+            window.alert(err);
+        });
+            event.preventDefault();
+            return false;
+       
+}
